96f4f072a5b90cd3d47a97b55fd72520069bb107
felipeferreira <felipefcvto@gmail.com>
2024-05-06 02:21:29 -0300

    add. estilos as págs. index e connection

--- app/connection.php
+++ app/connection.php
@@ -1,17 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/global.css">
+    <title>Testando conexão com DB</title>
+</head>
+<body>
+    <section class="container">
+        <?php
+        $servername = "db"; 
+        $username = "user"; 
+        $password = "password"; 
+        $database = "db"; 
 
-$servername = "db"; 
-$username = "user"; 
-$password = "password"; 
-$database = "db"; 
+        $conn = new mysqli($servername, $username, $password, $database);
 
-$conn = new mysqli($servername, $username, $password, $database);
+        if ($conn->connect_error) {
+            echo "<p>Erro ao conectar ao banco de dados: " . $conn->connect_error . "</p>";
+        } else {
+            echo "<h1>Conexão bem-sucedida! 🚀</h1>";
+        }
 
-if ($conn->connect_error) {
-    die("Erro ao conectar ao banco de dados: " . $conn->connect_error);
-} else {
-    echo "Conexão bem-sucedida ao banco de dados!";
-}
-
-$conn->close();
-?>
+        $conn->close();
+        ?>
+    </section>
+</body>
+</html>
--- app/index.php
+++ app/index.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/global.css">
+    <title>Bem-vindo ao Projeto Project-Two</title>
 </head>
 <body>
-    <a href="connection.php">Connection</a>
+    <div class="container">
+        <h1>Boa, deu certo!</h1>
+        <p>O servidor está rodando! Hora de testar o banco de dados...</p>
+        <a href="connection.php" class="btn">Testar conexão</a>
+    </div>
 </body>
 </html>
